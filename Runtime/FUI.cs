9f0c9a0b56baf1376eaa2db9fd99fb368130d7ac
Blank <wangfj11@foxmail.com>
2024-09-02 18:29:23 +0800

    [修改]1. 修改FGUI基类

--- Runtime/FUI.cs
+++ Runtime/FUI.cs
@@ -1,5 +1,4 @@
 ﻿using FairyGUI;
-using System;
 using GameFrameX.UI.Runtime;
 
 namespace GameFrameX.UI.FairyGUI.Runtime
@@ -9,7 +8,7 @@ namespace GameFrameX.UI.FairyGUI.Runtime
         /// <summary>
         /// UI 对象
         /// </summary>
-        public GObject GObject { get; }
+        public GObject GObject { get; private set; }
 
         /// <summary>
         /// 设置UI的显示状态，不发出事件
@@ -48,94 +47,10 @@ namespace GameFrameX.UI.FairyGUI.Runtime
                     return;
                 }
 
-                if (value == false)
-                {
-                    // OnHideBeforeAction?.Invoke(this);
-                    // foreach (var child in Children)
-                    // {
-                    //     ((FUI)child.Value).Visible = value;
-                    // }
-
-                    // OnHide();
-                    // OnHideAfterAction?.Invoke(this);
-                }
-
                 GObject.visible = value;
-                if (value)
-                {
-                    // OnShowBeforeAction?.Invoke(this);
-                    // foreach (var child in Children)
-                    // {
-                    //     ((FUI)child.Value).Visible = value;
-                    // }
-                    //
-                    // OnShow();
-                    // OnShowAfterAction?.Invoke(this);
-                    // Refresh();
-                }
             }
         }
 
-        /// <summary>
-        /// 销毁UI对象
-        /// </summary>
-        public override void Dispose()
-        {
-            base.Dispose();
-            // if (IsDisposed)
-            // {
-            //     return;
-            // }
-            //
-            // IsDisposed = true;
-            // // 删除所有的孩子
-            // DisposeChildren();
-            //
-            // // 删除自己的UI
-            // if (!IsRoot)
-            // {
-            //     RemoveFromParent();
-            // }
-            //
-            // // 释放UI
-            // OnDispose();
-            // // 删除自己的UI
-            // if (!IsRoot)
-            // {
-            //     GObject.Dispose();
-            // }
-            //
-            // Release(false);
-            // isFromFGUIPool = false;
-        }
-
-        /// <summary>
-        /// 添加UI对象到子级列表
-        /// </summary>
-        /// <param name="ui"></param>
-        /// <param name="index">添加到的目标UI层级索引位置</param>
-        /// <exception cref="Exception"></exception>
-        // protected override void AddInner(UI.Runtime.UI ui, int index = -1)
-        // {
-        //     var fui = (FUI)ui;
-        //     Children.Add(ui.Name, ui);
-        //     if (index < 0 || index > Children.Count)
-        //     {
-        //         GObject.asCom.AddChild(fui.GObject);
-        //     }
-        //     else
-        //     {
-        //         GObject.asCom.AddChildAt(fui.GObject, index);
-        //     }
-        //
-        //     fui.Parent = this;
-        //
-        //     if (fui.IsInitVisible)
-        //     {
-        //         // 显示UI
-        //         fui.Show(fui.UserData);
-        //     }
-        // }
 
         /// <summary>
         /// 设置当前UI对象为全屏
@@ -145,35 +60,13 @@ namespace GameFrameX.UI.FairyGUI.Runtime
             GObject?.asCom?.MakeFullScreen();
         }
 
-        /// <summary>
-        /// 删除指定UI名称的UI对象
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        /*public override bool Remove(string name)
-        {
-            if (Children.TryGetValue(name, out var ui))
-            {
-                Children.Remove(name);
-
-                if (ui != null)
-                {
-                    ui.RemoveChildren();
-
-                    ui.Hide();
-
-                    if (IsComponent)
-                    {
-                        GObject.asCom.RemoveChild(((FUI)ui).GObject);
-                    }
-
-                    return true;
-                }
-            }
-
-            return false;
-        }*/
         public FUI(GObject gObject, object userData = null, bool isRoot = false)
+        {
+            GObject = gObject;
+            InitView();
+        }
+
+        public void SetGObject(GObject gObject)
         {
             GObject = gObject;
         }
