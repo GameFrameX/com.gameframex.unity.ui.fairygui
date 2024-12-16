47589662cd1d8e69a5910265d580ea3dfad10174
Blank <wangfj11@foxmail.com>
2024-12-16 11:44:53 +0800

    [增加]1. 增加UI资源导入的配置

---
+++ Editor/UITextureAssetPostprocessor.cs
@@ -0,0 +1,25 @@
+﻿using GameFrameX.Runtime;
+using UnityEditor;
+
+namespace GameFrameX.UI.Editor
+{
+    internal sealed class UITextureAssetPostprocessor : AssetPostprocessor
+    {
+        void OnPreprocessTexture()
+        {
+            if (assetPath.Contains(PathHelper.Combine(Utility.Asset.Path.BundlesPath, "UI")))
+            {
+                TextureImporter textureImporter = (TextureImporter)assetImporter;
+                if (textureImporter.textureType != TextureImporterType.Default)
+                {
+                    textureImporter.textureType = TextureImporterType.Default;
+                }
+
+                if (textureImporter.mipmapEnabled)
+                {
+                    textureImporter.mipmapEnabled = false;
+                }
+            }
+        }
+    }
+}
